f683b0c6d1974025db3d5d9f9622b7c20f67e5cf
Jagmeet Singh Grewal <jugee.grewal@gmail.com>
2021-02-08 19:41:03 +1100

    Some spellings

--- index.js
+++ index.js
@@ -1,10 +1,17 @@
 const Discord = require('discord.js');
-const bot = new Discord.Client
+const bot = new Discord.Client();
 const { token } = require("./config.json");
 
 bot.on('ready', ()=>{
     console.log("Bot is ready");
 });
+
+bot.on('message', msg => {
+    if (msg.content === 'ping') {
+      msg.reply('pong');
+      msg.channel.send('pong');
+    }
+  });
 
 bot.login(token);
  
