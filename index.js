6c7409992fef0ef543dd9bb79c7e1cca57985417
Jagmeet Singh Grewal <jugee.grewal@gmail.com>
2021-02-13 21:20:26 +1100

    Added command folder. Modularise the commands. Event Class

--- index.js
+++ index.js
@@ -1,17 +1,33 @@
 const Discord = require('discord.js');
+const fs = require('fs');
 const bot = new Discord.Client();
 const { token } = require("./config.json");
+
+bot.commands = new Discord.Collection();
+
+const commandFiles = fs.readdirSync('./commands/').filter(file => file.endsWith('.js'))
+for(const file of commandFiles){
+    const command = require(`./commands/${file}`)
+    bot.commands.set(command.name, command)
+}
 
 bot.on('ready', ()=>{
     console.log("Bot is ready");
 });
 
 bot.on('message', msg => {
-    if (msg.content === 'ping') {
-      msg.reply('pong');
-      msg.channel.send('pong');
+    const args = msg.content.split(/ +/);
+    const command = args.shift().toLowerCase();
+    //console.info(`Called command: ${command}`);
+  
+    if (!bot.commands.has(command)) return;
+  
+    try {
+      bot.commands.get(command).execute(msg, args);
+    } catch (error) {
+      console.error(error);
+      msg.reply('there was an error trying to execute that command!');
     }
   });
 
-bot.login(token);
- 
+  bot.login(token);
